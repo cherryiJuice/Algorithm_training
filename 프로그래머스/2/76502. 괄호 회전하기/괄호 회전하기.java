062eee335ba5077b67127fe5221db43390d54752
cherryiJuice <143402486+cherryiJuice@users.noreply.github.com>
2024-04-07 18:28:47 +0900

    [level 2] Title: 괄호 회전하기, Time: 2.20 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        int cnt = 0;
+        String tmp = s;
+        char[] charArray = s.toCharArray();
+        int p = 0, q = 0, r = 0;
+        Stack<String> stack = new Stack<>();
+
+        for (char c : charArray) {
+            if (c == '(' || c == ')') p++;
+            if (c == '{' || c == '}') q++;
+            if (c == '[' || c == ']') r++;
+        }
+        if (p % 2 != 0 || q % 2 != 0 || r % 2 != 0) return 0;
+        System.out.println("tmp = " + tmp);
+        for (int i = 0; i < s.length(); i++) {
+            tmp = tmp.substring(1, s.length()) + tmp.substring(0, 1);
+            if (extracted(s, tmp, stack)) {
+                if (stack.isEmpty()) cnt++;
+            }
+            stack.clear();
+        }
+        return cnt;
+    }
+
+    private boolean extracted(String s, String tmp, Stack<String> stack) {
+        for (int j = 0; j < s.length(); j++) {
+            String str = String.valueOf(tmp.charAt(j));
+            if(str.equals("(") || str.equals("{") || str.equals("["))
+                stack.push(str);
+            else if(!stack.isEmpty()) {
+                if (str.equals(")")) {
+                    if (stack.peek().equals("(")) stack.pop();
+                } else if (str.equals("}")) {
+                    if (stack.peek().equals("{")) stack.pop();
+                } else if (str.equals("]")) {
+                    if (stack.peek().equals("[")) stack.pop();
+                } else return false;
+            }
+            else return false;
+
+        }
+        return true;
+    }
+}
