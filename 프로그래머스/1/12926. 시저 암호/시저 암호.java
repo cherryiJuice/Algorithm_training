8671d1e6fb829c66f41e9c0bb55d8a71bb60101e
cherryiJuice <143402486+cherryiJuice@users.noreply.github.com>
2024-07-08 20:25:45 +0900

    [level 1] Title: 시저 암호, Time: 1.54 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,13 +1,13 @@
 class Solution {
     public String solution(String s, int n) {
-        char[] chars = s.toCharArray();
+		char[] chars = s.toCharArray();
 		for (int i = 0; i < chars.length; i++) {
-			if(chars[i] >= 'A' && chars[i] <= 'Z') {
+			if(Character.isUpperCase(chars[i])) {
 				chars[i] = (char)(((chars[i]-'A' + n) % 26)+'A');
-			} else if(chars[i] >= 'a' && chars[i] <='z') {
-                chars[i] = (char)(((chars[i]-'a' + n) % 26)+'a');
+			} else if(Character.isLowerCase(chars[i])) {
+				chars[i] = (char)(((chars[i]-'a' + n) % 26)+'a');
 			}
 		}
 		return new String(chars);
-    }
+	}
 }
