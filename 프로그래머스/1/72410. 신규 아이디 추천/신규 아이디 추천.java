0bc80c076da47138ae4a2d27c15e73ad81eb32cd
cherryiJuice <143402486+cherryiJuice@users.noreply.github.com>
2024-07-09 17:41:06 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.78 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String new_id) {
+        while (true) {
+			new_id = new_id.toLowerCase();
+			new_id = new_id.replaceAll("[^a-z0-9._-]", "");
+			new_id = new_id.replaceAll("\\.{2,}", ".");
+			new_id = new_id.replaceAll("(^\\.)|(\\.$)", "");
+			if(new_id.isEmpty()){
+				new_id = "a";
+				continue;
+			}
+			if (new_id.length() > 15) {
+				new_id = new_id.substring(0, 15);
+				continue;
+			}
+			if (new_id.length() < 3) {
+				new_id += new_id.substring(new_id.length() - 1);
+				continue;
+			}
+			break;
+		}
+
+		return new_id;
+    }
+}
