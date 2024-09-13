f168cde870bef8be40a279607c01c4a1f37de6d7
cherryiJuice <143402486+cherryiJuice@users.noreply.github.com>
2024-09-13 18:43:52 +0900

    [level 2] Title: 기능개발, Time: 4.92 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        List<Integer> list = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        
+        int days = 0;
+        for(int i=0; i<progresses.length; i++) {
+            int temp = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
+            System.out.println(temp);
+            
+            if(days >= temp) {
+                list.set(list.size()-1, list.get(list.size()-1) + 1);
+            } else {
+                list.add(1);
+                days = temp;
+            }
+        }
+        return  list.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+}
