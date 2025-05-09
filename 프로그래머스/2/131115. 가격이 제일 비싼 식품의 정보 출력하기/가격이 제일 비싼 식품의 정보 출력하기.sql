95cb7be18578795f9b59fbb8df6dba48a9ec78c2
cherryiJuice <143402486+cherryiJuice@users.noreply.github.com>
2025-05-09 16:05:00 +0900

    [level 2] Title: 가격이 제일 비싼 식품의 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131115. 가격이 제일 비싼 식품의 정보 출력하기/가격이 제일 비싼 식품의 정보 출력하기.sql
+++ 프로그래머스/2/131115. 가격이 제일 비싼 식품의 정보 출력하기/가격이 제일 비싼 식품의 정보 출력하기.sql
@@ -1,3 +1,4 @@
 -- 코드를 입력하세요
-select * from food_product
-where price = (select max(price) from food_product)
+SELECT * from FOOD_PRODUCT
+where price = (select max(price) from FOOD_PRODUCT)
+limit 1
