7d7da1eda1e63db14d3da426be2fd3e6cea1f46f
cherryiJuice <143402486+cherryiJuice@users.noreply.github.com>
2025-05-25 21:34:38 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 17.65 ms, Memory: 147 MB -BaekjoonHub

---
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] sequence, int k) {
+        int[] answer = new int[2];
+        int len = Integer.MAX_VALUE;
+        int lt = 0;
+        int sum = 0;
+        
+        for(int rt=0; rt< sequence.length; rt++) {
+            sum += sequence[rt];
+            if(sum == k) {
+                if(len > rt - lt) {
+                    answer[0] = lt; answer[1] = rt;
+                    len = rt - lt;
+                }
+            } 
+            else if(sum > k) {
+                while(sum > k) {
+                    sum -= sequence[lt];
+                    lt++;
+                }
+                if(sum == k) {
+                    if(len > rt - lt) {
+                        answer[0] = lt; answer[1] = rt;
+                        len = rt - lt;
+                    }
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
