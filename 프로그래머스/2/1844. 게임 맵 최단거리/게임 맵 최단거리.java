e02148a544bf308845dbac56b677e759cf3a0b38
cherryiJuice <143402486+cherryiJuice@users.noreply.github.com>
2025-05-27 01:12:22 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.25 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,33 +1,32 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
-    
     public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
+        int[] dx = {0, 1, -1, 0};
+        int[] dy = {1, 0, 0, -1};
         
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {0, 0, 1}); 
+        int n = maps.length, m = maps[0].length;
+        Queue<int[]> q = new LinkedList<>(); // x, y, dist
         
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int x = current[0];
-            int y = current[1];
-            int distance = current[2];
+        if(maps[0][0] == 0) return -1;
+        
+        q.add(new int[]{0, 0, 1});
+        
+        while(!q.isEmpty()) {
+            int[] poll = q.poll();
+            int x = poll[0];
+            int y = poll[1];
+            int dist = poll[2];
             
             if (x == n - 1 && y == m - 1) {
-                return distance;
+                return dist;
             }
             
-            for (int i = 0; i < 4; i++) {
+            for(int i=0; i<4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                
                 if (nx >= 0 && ny >= 0 && nx < n && ny < m && maps[nx][ny] == 1) {
-                    queue.offer(new int[] {nx, ny, distance + 1});
+                    q.offer(new int[] {nx, ny, dist + 1});
                     maps[nx][ny] = 0; 
                 }
             }
