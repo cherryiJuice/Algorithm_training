e4abc3816c0ac3a7ae4944bde6bb9e53f2e68da9
cherryiJuice <143402486+cherryiJuice@users.noreply.github.com>
2025-05-30 00:40:53 +0900

    [level 1] Title: 신규 아이디 추천, Time: 6.16 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,40 +1,25 @@
-import java.util.*;
-
 class Solution {
     public String solution(String new_id) {
-        String answer;
-        // 1단계
-        answer = new_id.toLowerCase();
-        // 2단계
-        answer = answer.replaceAll("[^a-z0-9-_.]", "");
-        // 3단계
-        answer = answer.replaceAll("\\.{2,}", ".");
-        // 4단계
-        if(answer.indexOf(".") == 0) {
-            answer = answer.substring(1);
-        }
-        if(answer.lastIndexOf(".") == answer.length()-1 && answer.length() > 1) {
-            answer = answer.substring(0, answer.length()-1);
-        }
-        
-        // 5단계
-        if(answer == "" || answer.isEmpty()) {
-            answer = "a";
-        }
-        // 6단계
-        if(answer.length() >= 16) {
-            answer = answer.substring(0, 15);
-            if(answer.lastIndexOf(".") == answer.length()-1) {
-                answer = answer.substring(0, 14);
-            }
-        }
-        // 7단계
-        if(answer.length() <= 2) {
-            String last = answer.substring(answer.length()-1);
-            int count = 3 - answer.length();
-            answer += last.repeat(count);
-        }
-        System.out.println(answer);
-        return answer;
+        while (true) {
+					new_id = new_id.toLowerCase();
+					new_id = new_id.replaceAll("[^a-z0-9._-]", "");
+					new_id = new_id.replaceAll("\\.{2,}", ".");
+					new_id = new_id.replaceAll("(^\\.)|(\\.$)", "");
+					if(new_id.isEmpty()){
+						new_id = "a";
+						// continue;
+					}
+					if (new_id.length() > 15) {
+						new_id = new_id.substring(0, 15);
+						continue;
+					}
+					if (new_id.length() < 3) {
+						new_id += new_id.substring(new_id.length() - 1);
+						continue;
+					}
+					break;
+				}
+
+				return new_id;
     }
 }
