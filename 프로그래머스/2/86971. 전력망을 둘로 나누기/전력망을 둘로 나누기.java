354b3f514bf78beca7a7645863c62233897d26e9
cherryiJuice <143402486+cherryiJuice@users.noreply.github.com>
2025-05-30 11:24:05 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.09 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -3,45 +3,41 @@ import java.util.*;
 class Solution {
     List<List<Integer>> graph = new ArrayList<>();
     boolean[] visited;
-
+    
     public int solution(int n, int[][] wires) {
-        for (int i = 0; i <= n; i++) {
+        int answer = Integer.MAX_VALUE;
+        
+        for(int i=0; i<=n; i++) {
             graph.add(new ArrayList<>());
         }
-
-        for (int[] wire : wires) {
+        
+        for(int[] wire: wires) {
             int a = wire[0];
             int b = wire[1];
             graph.get(a).add(b);
             graph.get(b).add(a);
         }
-
-        int min = Integer.MAX_VALUE;
-
-        for (int[] wire : wires) {
+        
+        for(int[] wire: wires) {
             int a = wire[0];
             int b = wire[1];
-
-            visited = new boolean[n + 1];
-            int subtreeSize = dfs(a, b); // b를 끊은 것처럼 처리
-            int other = n - subtreeSize;
-
-            min = Math.min(min, Math.abs(subtreeSize - other));
+            visited = new boolean[n+1];
+            
+            int sub = dfs(a, b);
+            int other = n - sub;
+            answer = Math.min(answer, Math.abs(other - sub));
         }
-
-        return min;
+        return answer;
     }
-
-    private int dfs(int current, int cut) {
-        visited[current] = true;
+    
+    private int dfs(int cur, int cut) {
         int count = 1;
-
-        for (int next : graph.get(current)) {
-            if (!visited[next] && next != cut) {
+        visited[cur] = true;
+        for(int next : graph.get(cur)) {
+            if(!visited[next] && next != cut) {
                 count += dfs(next, cut);
             }
         }
-
         return count;
     }
 }
