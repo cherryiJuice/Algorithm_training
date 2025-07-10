0d16a4ec5698c70f5c58a8767957b23fe55e3775
cherryiJuice <143402486+cherryiJuice@users.noreply.github.com>
2025-07-10 23:55:47 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 616 ms, Memory: 142080 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -2,54 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static List<List<Integer>> graph;
-    static boolean[] visited;
+	static List<List<Integer>> graph;
+	static boolean[] visited;
 
-    public static void main(String[] args) throws IOException {
-        // 빠른 입력
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // 정점 수, 간선 수 입력
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 
-        // 인접 리스트 초기화
-        graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
-        }
+		graph = new ArrayList<>();
+		for (int i = 0; i <= n; i++) {
+			graph.add(new ArrayList<>());
+		}
 
-        // 간선 정보 입력
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
+		// 간선 정보 입력
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
 
-            graph.get(u).add(v);
-            graph.get(v).add(u);  // 무방향 그래프
-        }
+			graph.get(u).add(v);
+			graph.get(v).add(u);  // 무방향 그래프
+		}
 
-        visited = new boolean[n + 1];
-        int count = 0;
+		visited = new boolean[n + 1];
+		int count = 0;
 
-        // 연결 요소 개수 세기
-        for (int i = 1; i <= n; i++) {
-            if (!visited[i]) {
-                dfs(i);
-                count++;
-            }
-        }
+		// 연결 요소 개수 세기
+		for (int i = 1; i <= n; i++) {
+			if (!visited[i]) {
+				dfs(i);
+				count++;
+			}
+		}
 
-        System.out.println(count);
-    }
+		System.out.println(count);
+	}
 
-    static void dfs(int node) {
-        visited[node] = true;
-        for (int next : graph.get(node)) {
-            if (!visited[next]) {
-                dfs(next);
-            }
-        }
-    }
+	static void dfs(int node) {
+		visited[node] = true;
+		for (int next : graph.get(node)) {
+			if (!visited[next]) {
+				dfs(next);
+			}
+		}
+	}
 }
+
