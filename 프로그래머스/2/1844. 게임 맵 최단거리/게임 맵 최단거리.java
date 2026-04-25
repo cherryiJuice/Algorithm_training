a0356ade5281cd04c5e93d400f055ee926806b96
cherryiJuice <143402486+cherryiJuice@users.noreply.github.com>
2026-04-25 14:29:06 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.93 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,37 +1,46 @@
 import java.util.*;
-
 class Solution {
+    int[] dx = {0, 1, -1, 0};
+    int[] dy = {1, 0, 0, -1};
+    
     public int solution(int[][] maps) {
-        int[] dx = {0, 1, -1, 0};
-        int[] dy = {1, 0, 0, -1};
         
-        int n = maps.length, m = maps[0].length;
-        Queue<int[]> q = new LinkedList<>(); // x, y, dist
+        int answer = Integer.MAX_VALUE;
+        int n = maps.length;
+        int m = maps[0].length;
         
-        if(maps[0][0] == 0) return -1;
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[n][m];
         
         q.add(new int[]{0, 0, 1});
         
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             int[] poll = q.poll();
             int x = poll[0];
             int y = poll[1];
-            int dist = poll[2];
-            
-            if (x == n - 1 && y == m - 1) {
-                return dist;
-            }
+            int distance = poll[2];
             
             for(int i=0; i<4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                if (nx >= 0 && ny >= 0 && nx < n && ny < m && maps[nx][ny] == 1) {
-                    q.offer(new int[] {nx, ny, dist + 1});
-                    maps[nx][ny] = 0; 
+                int nextD = distance + 1;
+                
+                if(nx == n-1 && ny == m-1) {
+                    answer = Math.min(answer, nextD);
+                }
+                
+                if(nx<0 || ny<0 || nx>=n || ny>=m || maps[nx][ny] == 0) continue;
+                
+                
+                if(!visited[nx][ny]) {
+                    q.add(new int[] {nx, ny, nextD});
+                    visited[nx][ny] = true;
                 }
             }
         }
         
-        return -1;
+        if(answer == Integer.MAX_VALUE) return -1;
+        
+        return answer;
     }
 }
