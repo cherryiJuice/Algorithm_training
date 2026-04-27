6d213cba921ca4b9138910ba45a707e04f043c95
cherryiJuice <143402486+cherryiJuice@users.noreply.github.com>
2026-04-28 00:51:51 +0900

    [level 2] Title: 후보키, Time: 7.34 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.java
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -1,46 +1,53 @@
 import java.util.*;
 
 class Solution {
-    Set<Set<Integer>> keys = new HashSet<>();
+    List<Set<Integer>> candidateKeys = new ArrayList<>();
 
     public int solution(String[][] relation) {
-        int colCount = relation[0].length;
-
-        for (int i = 1; i <= colCount; i++) {
-            dfs(0, i, colCount, new LinkedHashSet<>(), relation);
+        int colLen = relation[0].length;
+        
+        // 컬럼의 조합을 1개부터 colLen개까지 탐색
+        for (int i = 1; i <= colLen; i++) {
+            dfs(relation, new LinkedHashSet<>(), 0, i);
         }
-
-        return keys.size();
+        
+        return candidateKeys.size();
     }
 
-    // start: 시작 인덱스, targetSize: 조합 크기
-    private void dfs(int start, int targetSize, int colCount, Set<Integer> candidate, String[][] relation) {
-        if (candidate.size() == targetSize) {
-            // 유일성 검사
-            Set<String> set = new HashSet<>();
-            for (String[] row : relation) {
-                StringBuilder sb = new StringBuilder();
-                for (int col : candidate) {
-                    sb.append(row[col]).append("|");
-                }
-                set.add(sb.toString());
+    void dfs(String[][] relation, Set<Integer> temp, int start, int size) {
+        // 목표로 하는 조합의 길이에 도달했을 때
+        if (temp.size() == size) {
+            if (isUnique(temp, relation) && isMinimal(temp)) {
+                candidateKeys.add(new HashSet<>(temp));
             }
-
-            if (set.size() == relation.length) {
-                // 최소성 검사
-                for (Set<Integer> key : keys) {
-                    if (candidate.containsAll(key)) return;
-                }
-                keys.add(new HashSet<>(candidate));
-            }
-
             return;
         }
 
-        for (int i = start; i < colCount; i++) {
-            candidate.add(i);
-            dfs(i + 1, targetSize, colCount, candidate, relation);
-            candidate.remove(i);
+        for (int i = start; i < relation[0].length; i++) {
+            temp.add(i);
+            dfs(relation, temp, i + 1, size);
+            temp.remove(i);
         }
+    }
+
+    // 유일성 검사: 모든 행을 구분할 수 있는가?
+    boolean isUnique(Set<Integer> temp, String[][] relation) {
+        Set<String> set = new HashSet<>();
+        for (String[] row : relation) {
+            StringBuilder sb = new StringBuilder();
+            for (int col : temp) {
+                sb.append(row[col]).append("/");
+            }
+            set.add(sb.toString());
+        }
+        return set.size() == relation.length;
+    }
+
+    // 최소성 검사: 현재 조합이 기존 후보키를 포함하고 있는가?
+    boolean isMinimal(Set<Integer> temp) {
+        for (Set<Integer> key : candidateKeys) {
+            if (temp.containsAll(key)) return false;
+        }
+        return true;
     }
 }
